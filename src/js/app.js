0ce1b24297723930e508277fdd3409561e2408d0
Forrest Wilson <forrest.wilson@me.com>
2018-01-28 22:37:28 +1300

    Added show/hide nav events

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
-    // Variables
+    //**** Variables ****//
+
     var transitionSpeed = 400;
+
+    //**** Event Functions ****//
+
+    function showNav() {
+        $("#mobileNavDropdown").fadeIn(transitionSpeed);
+        $("#frame").addClass("blur");
+    }
+
+    function hideNav() {
+        $("#mobileNavDropdown").fadeOut(transitionSpeed);
+        $("#frame").removeClass("blur");
+    }
+
+    //**** Events Triggers ****//
+
+    $("#hamburgerButton").on("click", function() {
+        showNav();
+    });
+
+    $("#closeNavButton").on("click", function() {
+        hideNav();
+    });
 });
